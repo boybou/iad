a322e366b8cd8e7ae9d97cff4d58ce33f528973b
boy <boy.bouwense@gmail.com>
2018-05-30 19:38:37 +0200

    week 1

--- src/RijBewerkingen.java
+++ src/RijBewerkingen.java
@@ -18,34 +18,41 @@ public class RijBewerkingen
 	public int lineair_zoeken(char target)
 	{
 		int index = 0;
-		while(target != rij[index]){
-
+		while(index < rij.length){
+			if(target == rij[index]){
+				return index;
+			}
 			index++;
 		}
-		return index;
+		return -1;
 	}
 	
 	
 	// als target voorkomt in de gesorteerde rij dan wordt de index van target 
 	// teruggegeven; als target niet voor komt dan -1.
-//	public int binair_zoeken(char target)
-//	{
-////		// nog afmaken ...
-////		int top = ....................;
-////		int bottom = 0;
-////		while (bottom <= top)
-////		{
-////			int mid = .....................;
-////			if (rij[mid] == target)
-////			     return (mid);
-////			else
-////			     if (.......................)
-////			  	top = mid - 1;
-////			     else
-////			  	.....................;
-////		}
-////		return (-1);
-//	}
+	public int binair_zoeken(char target)
+	{
+		int top = this.rij.length;
+		int bottom = 0;
+		while (bottom <= top)
+		{
+			int mid = bottom+((top-bottom)/2);
+//			System.out.println(mid);
+			if (rij[mid-1] == target)
+			     return (mid);
+			else
+			     if (rij[mid] > target){
+			top = mid - 1;
+		}
+			     else{
+			     	top = this.rij.length;
+			     	bottom= mid;
+
+		}
+
+		}
+		return (-1);
+	}
 
 	
 	// de rij wordt in oplopende volgorde gesorteerd
--- src/TestMain.java
+++ src/TestMain.java
@@ -11,6 +11,10 @@ public class TestMain {
 		}
 		RijBewerkingen test = new RijBewerkingen(rij);
 		System.out.println(test.lineair_zoeken(rij[4]));
+		char [] rij2 = new char[] {'1','2','5','7','8','9'};
+		RijBewerkingen test2 = new RijBewerkingen(rij2);
+		System.out.println(test2.binair_zoeken('5'));
+
 
 
 	}
